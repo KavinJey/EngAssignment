255fc642e11f4a8e9ea18d4c927411a5fc91b170
Jeyakavin Jeyaranjan <jeyakavin33@gmail.com>
2018-03-07 20:24:04 -0500

    Update settings.py

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -101,4 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/news/static/'
-STATIC_ROOT = "/news/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "news/static")
