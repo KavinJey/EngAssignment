3b0bb0a0d06a7b7051d617d2b3215efdde25de91
KavinJey <jeyakavin33@gmail.com>
2018-03-07 20:36:50 -0500

    weas

--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
+from django.conf import settings
 from . import views
 
 
@@ -8,3 +9,8 @@ urlpatterns = [
     url(r'^feeds/', views.feeds_list, name='feeds_list')
 
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
