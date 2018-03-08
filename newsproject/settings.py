d41acf38c1c9d1f871672ea696225bc57aaa9153
Jeyakavin Jeyaranjan <jeyakavin33@gmail.com>
2018-03-07 19:03:57 -0500

    static

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -100,6 +100,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+STATIC_URL = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
